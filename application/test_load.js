358df4bd40c12b556abf86fc228e32b3131a4da9
abduljcerim reka <abduljcerim.reka@gmail.com>
2017-12-02 18:36:00 +0100

    electron init by are

--- application/test_load.js
+++ application/test_load.js
@@ -10,4 +10,16 @@ function test_load() {
         DurationTime
         WorkerCount
     */
+    var uri = document.getElementById('uriTxtBox').value;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", uri, true ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+    
+    //document.getElementById('view')
+    ///.exectueJavaScrpipt('fetch("http://10.0.0.32:8080", {method: "get"})');
+  //.executeJavaScript('fetch("http://example.com/?foo=bar", {method: "post"});');
+ 
+
+  
 }
--- application/test_statistics.js
+++ application/test_statistics.js
@@ -8,4 +8,15 @@ function test_statistics() {
         Liste von LoadTests
         Auswertung
     */
+
+
+    var arr = [ {"url":"google.at", "successful": "c0.85", "totalTime":"231"}, 
+    {"url":"facebook.com", "successful": "0.74", "totalTime":"100"}];
+    
+    var html = '';
+
+    // todo
+
+        document.querySelector('#table > tbody').innerHTML = html;
+    
 }
